8c7c6f985fdc1fffc41f7f0b3ab3d167a43d4cd3
Wiktor Gała <wiktorgala@Wiktors-MacBook-Pro.local>
2020-06-18 13:06:27 +0200

    feat(Products): Products section heading and caption

--- src/components/HomePage/Products/index.js
+++ src/components/HomePage/Products/index.js
@@ -1,8 +1,38 @@
+/** @jsx jsx */
 import React from "react"
+import { jsx, Styled } from "theme-ui"
+import { Row, Col } from "../../Grid"
 
 const HomepageProducts = () => (
-  <div>
-  </div>
+  <section sx={{ paddingTop: [60, 60, 105] }}>
+    <Row>
+      <Col styles={styles.headingWrapper}>
+        <Styled.h2 sx={styles.heading}>
+          Explore community choices
+        </Styled.h2>
+        <Styled.p>
+          Updated daily based on most popular choices
+          <br />
+          among dev community
+        </Styled.p>
+      </Col>
+    </Row>
+  </section>
 )
 
 export default HomepageProducts
+
+const styles = {
+  headingWrapper: {
+    textAlign: ["center", null, "left"],
+    br: {
+      display: ["none", "inherit"],
+    },
+  },
+  heading: {
+    variant: "text.heading",
+    fontFamily: "body",
+    fontWeight: "bold",
+    marginBottom: "20px",
+  },
+}
