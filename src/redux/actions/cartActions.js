36dc1576a2ba8e83f4ca32d4ad777f22bddcd972
Wiktor Gała <wiktorgala@Wiktors-MacBook-Pro.local>
2020-06-19 17:41:43 +0200

    feat(Cart): create actions

---
+++ src/redux/actions/cartActions.js
@@ -0,0 +1,7 @@
+export const TOGGLE_CART = "TOGGLE_CART"
+export const OPEN_CART = "OPEN_CART"
+export const CLOSE_CART = "CLOSE_CART"
+
+export const toggleCart = () => ({ type: TOGGLE_CART })
+export const openCart = () => ({ type: OPEN_CART })
+export const closeCart = () => ({ type: CLOSE_CART })
--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,3 +1,9 @@
+import {
+  TOGGLE_CART,
+  OPEN_CART,
+  CLOSE_CART,
+} from "./../actions/cartActions"
+
 const initialState = {
   isVisible: false,
   items: [],
@@ -5,6 +11,21 @@ const initialState = {
 
 function cartReducer(state = initialState, action) {
   switch (action.type) {
+    case TOGGLE_CART:
+      return {
+        ...state,
+        isVisible: !state.isVisible,
+      }
+    case OPEN_CART:
+      return {
+        ...state,
+        isVisible: true,
+      }
+    case CLOSE_CART:
+      return {
+        ...state,
+        isVisible: false,
+      }
     default:
       return state
   }
