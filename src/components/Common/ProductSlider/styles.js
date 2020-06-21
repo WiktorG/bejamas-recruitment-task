d8c2dcafb2efa8396b0e059f044ab95c6d55841d
Wiktor Gała <wiktorgala@Wiktors-MacBook-Pro.local>
2020-06-21 14:28:04 +0200

    feat(Product Slider): full width on 1440

--- src/components/Common/ProductSlider/styles.js
+++ src/components/Common/ProductSlider/styles.js
@@ -3,11 +3,18 @@ export default {
     position: "relative",
     px: ["50px", null, "20px", null, 0],
     marginTop: [40, 64],
+    ".slick-list": {
+      mx: ["20px", null, null, null, "-15px", null, 0],
+    },
     ".slick-slide": {
       outline: "none",
     },
+    ".slick-slide > div": {
+      mx: ["20px", null, null, null, "15px", null, 0],
+    },
   },
   arrow: {
+    position: "relative",
     height: "auto",
     width: ["50px", null, null, "55px", null, "60px", "65px"],
   },
@@ -18,7 +25,7 @@ export default {
     position: "absolute",
     right: "100%",
     top: "50%",
-    transform: ["translate(45px, -50%)", null, "translate(35px, -50%)", null, "translate(-10px, -50%)", "translate(-15px, -50%)"],
+    transform: ["translate(50px, -50%)", null, "translate(40px, -50%)", null, "translate(-10px, -50%)", "translate(-15px, -50%)"],
     cursor: "pointer",
     outline: "none",
   },
@@ -29,7 +36,7 @@ export default {
     position: "absolute",
     left: ["100%"],
     top: "50%",
-    transform: ["translate(-45px, -50%)", null, "translate(-35px, -50%)", null, "translate(10px, -50%)", "translate(15px, -50%)"],
+    transform: ["translate(-50px, -50%)", null, "translate(-40px, -50%)", null, "translate(10px, -50%)", "translate(15px, -50%)"],
     cursor: "pointer",
     outline: "none",
   },
--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -6,7 +6,7 @@ import {
 } from "./../actions/cartActions"
 
 const initialState = {
-  isVisible: true,
+  isVisible: false,
   items: [],
 }
 
