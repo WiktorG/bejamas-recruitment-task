df3c3104a0b9362f6299573619e2a7875fc4a78e
Wiktor Gała <wiktorgala@Wiktors-MacBook-Pro.local>
2020-06-21 11:52:10 +0200

    feat(Cart): close cart on page change

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,17 +1,24 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
 // eslint-disable-next-line no-unused-vars
-import React from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { Global } from "@emotion/core"
 
+import { closeCart } from "./../../redux/actions/cartActions"
 import { cart as cartSelector } from "./../../redux/selectors/cartSelectors"
 
 import Header from "../Header"
 
 export default function Layout({ children }) {
+  const dispatch = useDispatch()
   const { isVisible } = useSelector(cartSelector)
+
+  // close cart on page change
+  useEffect(() => {
+    dispatch(closeCart())
+  }, [])
 
   return (
     <>
